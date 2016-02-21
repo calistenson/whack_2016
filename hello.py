84dd97a8b1f011014591918e75af5e1f24729b8d
calistenson <cstenson@wellesley.edu>
2016-02-20 21:02:19 -0500

    Updated form submit and redirect. Added available seats page.

--- hello.py
+++ hello.py
@@ -13,6 +13,24 @@ def index():
     """
     return flask.render_template('index.html')
 
+@APP.route('/about')
+def about():
+    """ Displays the about page accessible at '/about'
+    """
+    return flask.render_template('about.html')
+
+@APP.route('/available')
+def available():
+    """ Displays the about page accessible at '/available'
+    """
+    return flask.render_template('available.html')
+
+@APP.route('/', methods = ['POST'])
+def seats():
+    num_seats = flask.request.form['num_seats']
+    print("The number of seats is '" + num_seats + "'")
+    return flask.redirect('/available')
+
 
 if __name__ == '__main__':
     APP.debug=True
